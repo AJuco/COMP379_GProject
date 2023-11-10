885692b13eb7f865b353a4865de93e8a6932bf38
AJuco <94945960+AJuco@users.noreply.github.com>
2023-11-10 15:40:30 -0600

    Add files via upload
    Here's the final preprocessed data

---
+++ Preprocessing.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+#  from sklearn.model_selection import train_test_split
+#  from sklearn.linear_model import LogisticRegression
+#  from sklearn.metrics import accuracy_score
+
+df = pd.read_csv('Sleep_health_and_lifestyle_dataset.csv')
+LE = LabelEncoder()
+
+"""
+print(df.isnull().sum())
+print(f'\nNumber of duplicates: {df.duplicated().sum()}\n')
+print(df.dtypes)
+"""
+# NO DUPLICATE, NULL, OR EMPTY DATA
+
+# Converting categorical data to numerical
+df['Gender'] = LE.fit_transform(df['Gender'])
+df['Occupation'] = LE.fit_transform(df['Occupation'])
+df['BMI Category'] = LE.fit_transform(df['BMI Category'])
+df['Blood Pressure'] = LE.fit_transform(df['Blood Pressure'])
+df['Sleep Disorder'] = LE.fit_transform(df['Sleep Disorder'])
+
+# Preprocessing
+MMS = MinMaxScaler()  # Used for normalization
+SS = StandardScaler()  # Used for Standardization
+
+df['Age'] = SS.fit_transform(df[['Age']])
+df['Sleep Duration'] = SS.fit_transform(df[['Sleep Duration']])
+df['Quality of Sleep'] = MMS.fit_transform(df[['Quality of Sleep']])
+df['Physical Activity Level'] = MMS.fit_transform(df[['Physical Activity Level']])
+df['Stress Level'] = MMS.fit_transform(df[['Stress Level']])
+df['Blood Pressure'] = SS.fit_transform(df[['Blood Pressure']])
+df['Heart Rate'] = SS.fit_transform(df[['Heart Rate']])
+df['Daily Steps'] = SS.fit_transform(df[['Daily Steps']])
+
+# Extracting X data and Y data
+X_data = df[df.columns.drop(['Sleep Disorder'])].values
+Y_data = df['Sleep Disorder'].values
