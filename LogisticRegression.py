add0c569ef876602bbee07a6088d38aab66caa93
klsalerno <ksalerno@luc.edu>
2023-11-18 11:30:15 -0600

    Changed logistic regression classifier to use a validation/development set to select C hyperparameter

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
@@ -51,6 +53,7 @@ sc = StandardScaler()
 X_train_scaled = sc.fit_transform(X_train_res)
 X_test_scaled = sc.transform(X_test)
 
+'''
 logreg = LogisticRegression()
 logreg.fit(X_train_scaled, y_train)
 y_pred = logreg.predict(X_test_scaled)
@@ -58,3 +61,52 @@ c_matrix = confusion_matrix(y_test, y_pred)
 print(c_matrix)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'The accuracy of logistic regression on the test data is: {round(accuracy, 4)}')
+'''
+
+############
+
+X_train_temp, X_test, y_train_temp, y_test_dev = train_test_split(X_data, Y_data, test_size=0.15, random_state=0)
+sm = SMOTE(random_state=0)
+X_train_res, y_train_res = sm.fit_resample(X_train_temp, y_train_temp.ravel())
+X_train, X_valid, y_train_dev, y_valid_dev = train_test_split(X_train_res, y_train_res, test_size=(.15 / .85),
+                                                              random_state=0)
+sc = StandardScaler()
+X_train_scaled_dev = sc.fit_transform(X_train)
+X_valid_scaled_dev = sc.transform(X_valid)
+X_test_scaled_dev = sc.transform(X_test)
+
+logreg_default = LogisticRegression(random_state=0)
+logreg_default.fit(X_train_scaled_dev, y_train_dev)
+y_pred_valid_default = logreg_default.predict(X_valid_scaled_dev)
+
+accuracy_default = accuracy_score(y_valid_dev, y_pred_valid_default)
+print(f'Accuracy score of logistic regression using default hyperparameters on the validation set: {accuracy_default}')
+
+c_s = [0.0001, 0.001, 0.01, 0.1, 10, 100, 1000, 10000]
+c_improve_accuracy = 0
+c_improve = 0
+
+for c in c_s:
+    logreg_temp = LogisticRegression(C=c)
+    logreg_temp.fit(X_train_scaled_dev, y_train_dev)
+    y_pred_temp = logreg_temp.predict(X_valid_scaled_dev)
+    accuracy_temp = accuracy_score(y_valid_dev, y_pred_temp)
+    if accuracy_temp > c_improve_accuracy:
+        c_improve_accuracy = accuracy_temp
+        c_improve = c
+
+logreg_improve = LogisticRegression(C=c_improve, max_iter=500)
+logreg_improve.fit(X_train_scaled_dev, y_train_dev)
+y_pred_valid_improve = logreg_improve.predict(X_valid_scaled_dev)
+accuracy_improve = accuracy_score(y_valid_dev, y_pred_valid_improve)
+print(f'Accuracy score of logistic regression on the validation set with C = {c_improve} is {accuracy_improve}')
+
+X_train_combined = np.concatenate((X_train_scaled_dev, X_valid_scaled_dev))
+y_train_combined = np.concatenate((y_train_dev, y_valid_dev))
+logreg = LogisticRegression(C=c_improve, max_iter=500)
+logreg.fit(X_train_combined, y_train_combined)
+y_pred = logreg.predict(X_test_scaled_dev)
+accuracy = accuracy_score(y_test_dev, y_pred)
+print(f'Accuracy score of logistic regression on the test set with C = {c_improve} is {accuracy}')
+
+# Note for the report on why the accuracy is lower --> see kayla's personal testing file
